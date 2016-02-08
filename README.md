0bfb738adcfbc7a79544c6ce533ba48e84bc3d85
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2016-02-08 13:43:51 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+This is a wordpress theme starter theme combining the _tk theme with a grunt build
+
+The starter theme _tk was used as a foundation: https://github.com/Themekraft/_tk/
+
+The grunt build includes sass, concat, watch
+
+if you are looking for a starter worpress theme that entegrates bootstrap and grunt, try this out!
+
+
+Dependancies:
+
+Grunt.js http://gruntjs.com/
+Node.js https://nodejs.org/en/
+Sass http://sass-lang.com/
+
+1. make sure node.js and grunt.js are installed globally on your machine
+2. in the command line, go to your project folder
+3. npm init
+4. npm install grunt --save dev
+5. install grunt plugins:
+
+npm install grunt-contrib-concat --save-dev
+
+npm install grunt-contrib-sass --save-dev
+
+npm install grunt-contrib-watch --save-dev
+
+6. install sass (on mac):
+
+sudo gem install sass 
---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+
+		concat 		: { //task name: use cmd 'grunt concat' in terminal to excecute
+			options	: {
+				seperator: '\n\n//-----------------------------------------\n',
+				banner: '\n\n//---------------------------------------------\n'
+			},
+			dist	: {
+				src	: ['components/scripts/*.js'],//all js scripts in folder
+				dest: 'js/script.js'
+			}
+		}, //concat
+
+		sass 		: {
+			dist	: {
+				options : {
+					style: 'compressed'
+				},
+				files	: {
+					'style.css': 'components/sass/style.scss'
+				}
+			}
+		}, //sass
+
+		watch 		: {
+			options	: {
+				livereload: 35729
+			},
+			scripts: {
+					files: ['**/*.php',
+					'components/scripts/**/*.js',
+					'components/sass/**/*.scss'],
+					tasks: ['concat', 'sass']
+			}
+		} //watch
+
+	}); //initConfig function
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-bower-concat');
+	grunt.loadNpmTasks('grunt-wiredep');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	grunt.registerTask('default', [ 'sass', 'concat', 'watch']); //shortcut: use cmd 'grunt' in terminal to run tasks listed
+
+};// wrapper function
