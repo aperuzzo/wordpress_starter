22f467ce5cfce50bc9e7e904e1f2667fa5c7611e
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2016-03-10 10:15:21 -0600

    remove unused loadNPMtasks, added all scss files to watch function

--- gruntfile.js
+++ gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
 			scripts: {
 					files: ['**/*.php',
 					'components/scripts/**/*.js',
-					'components/sass/**/*.scss'],
+					'**/*.scss'],
 					tasks: ['concat', 'sass']
 			}
 		} //watch
@@ -39,8 +39,6 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-bower-concat');
-	grunt.loadNpmTasks('grunt-wiredep');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 
